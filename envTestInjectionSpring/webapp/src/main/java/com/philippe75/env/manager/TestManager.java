fc4b723d6bd211659eb0f6b2623d52a35a5adec2
Philippe Plaxine <philippe.plaxine02@gmail.com>
2018-10-27 10:45:18 +0200

    Résolution du problème d'injection

--- envTestInjectionSpring/webapp/src/main/java/com/philippe75/env/manager/TestManager.java
+++ envTestInjectionSpring/webapp/src/main/java/com/philippe75/env/manager/TestManager.java
@@ -1,12 +1,12 @@
 package com.philippe75.env.manager;
 
-import org.springframework.stereotype.Component;
+import javax.inject.Named;
 
-
+@Named
 public class TestManager {
 	
 	public String getString() {
-		String str = "Hello you!";
+		String str = "Victoire !!!! Abdoulail l'hérisson.";
 		return str;
 	}
 
--- envTestInjectionSpring/webapp/src/main/java/com/philippe75/env/servlets/Main.java
+++ envTestInjectionSpring/webapp/src/main/java/com/philippe75/env/servlets/Main.java
@@ -2,43 +2,43 @@ package com.philippe75.env.servlets;
 
 import java.io.IOException;
 
+import javax.inject.Inject;
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.web.context.support.SpringBeanAutowiringSupport;
+
 import com.philippe75.env.manager.TestManager;
 
+@Configurable
 public class Main extends HttpServlet {
 
 
+	@Inject
+	private TestManager testManager;
 	
-	public TestManager testManager;
-
 	public void setTestManager(TestManager testManager) {
 		this.testManager = testManager;
 	}
 
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		super.init(config);
+		SpringBeanAutowiringSupport.processInjectionBasedOnCurrentContext(this);
+	}
 
-	
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		String str;
-		if( testManager.getString() != null) {
-			str ="pas vide";
-		}else{
-			str ="vide";
-		}
 		
-		String test2 = str;
 		
-		//String test2 = testDao.getCountTest2();
-		//if(test2 != null) {
-			//System.out.println(test2);
-		//}else {
-			//System.out.println("Empty!!!!!");
-		//}
+		
+		String test2 = testManager.getString();
+		
 	
 		request.setAttribute( "test2", test2 );
 		
